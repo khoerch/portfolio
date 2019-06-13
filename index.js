7e1aa4cbd877dff32ce21bad460cda5d9359a69e
Kevin Hoercher <khoerch@gmail.com>
2019-06-13 13:31:42 -0400

    saving before branching

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 'use strict';
 
-function menuExpand {
+function menuExpand() {
     // Expand the nav items when the hamburger icon is clicked
 
 }
 
-function navScroll {
+function navScroll() {
     // Scroll to the appropriate element when nav item is clicked
-    $('#js-button-project').click(function(event) {
-        $('body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
-        }, 600);
-    })
 
 }
 
-function handlePage {
+function handlePage() {
     menuExpand();
     navScroll();
 }
