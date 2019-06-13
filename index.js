3b41e1b89cd9148ddd224478a5a43d41c559ecdd
Kevin Hoercher <khoerch@gmail.com>
2019-06-13 19:45:21 -0400

    last designs for full screen header

--- index.js
+++ index.js
@@ -9,13 +9,9 @@ function menuExpand() {
 
 function menuCollapse() {
     // Hide the nav items when a nav item is clicked
-    let windowWidth = $(window).width();
-
-    if(windowWidth < 850) {
-        $('#js-nav-item').click(function() {
-            $('.nav-bottom').toggleClass('hidden');
-        })
-    }
+    $('.js-nav-item').click(function() {
+        $('.nav-bottom').toggleClass('hidden');
+    });
 }
 
 function handlePage() {
